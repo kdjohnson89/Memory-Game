fe6887159e189a8985963e798e26320b7f9df64c
Kayla Johnson <kdjohnson89@users.noreply.github.com>
2018-06-06 21:21:20 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -168,13 +168,13 @@ let span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
+};
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+};
 //Play again button will clear modal and reset timer
 document.querySelector('.button').addEventListener('click', playAgain);
 document.querySelector('.button').addEventListener('click', resetTimer);
